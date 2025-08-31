802a8936e60280f5f7b9c5409ee621b624928f52
sondosEssam <sondosessam713@gmail.com>
2025-08-31 15:18:37 +0300

    last edits

--- javaScript/lab6-Events&Validation/form_validate/main.js
+++ javaScript/lab6-Events&Validation/form_validate/main.js
@@ -19,35 +19,69 @@ var tableData = [];
 
 
 //validation
-function validateName(){
-var nameregex = /[a-z]{3,25}/;
-if(!nameregex.test(fname.value))
-   {
-    name_inv.style.display= "block";
-    return(false);
-   }
+// function validateName(){
+// var nameregex = /[a-z]{3,25}/;
+// if(!nameregex.test(fname.value))
+//    {
+//     name_inv.style.display= "block";
+//     return(false);
+//    }
+//    name_inv.style.display = "none";
+//    return(true);
+// }
+
+
+function validateNameInput(value){
+    var nameregex = /[a-z]{3,25}/;
+    if(fname.value =="")
+    {
+        name_inv.innerHTML = "you must enter a name";
+        name_inv.style.display= "block";
+        return false;
+    }
+    if(!nameregex.test(value))
+    {
+        name_inv.innerHTML = "Name must be between 3 and 25 letters"
+        name_inv.style.display= "block";
+        fname.style.border = "2px solid red"
+        // border
+        return false;
+    }
+        fname.style.border = "2px solid green"
    name_inv.style.display = "none";
-   return(true);
+   // border
+   return true;
 }
 
-function validateid(){
+
+function validateid(value){
+    if(id.value=="")
+    {
+        id_inv.style.display= "block";
+        return false;
+    }
 for(var i = 0; i<tableData.length;i++){
-    if(id.value == tableData[i].id)
+    if(value == tableData[i].id)
             {
             id_inv.style.display = "block";
+            id.style.border = "2px solid red";
             return false;
-            }
-}
-id_inv.style.display = "none";
+        }
+    }
+    id.style.border = "2px solid green";
+    id_inv.style.display = "none";
 return true
 }
 
 function formValidate(){
-    if(validateName() && validateid())
+    var cnt= 0;
+    validateNameInput(fname.value)==true? cnt++:cnt=0;
+    validateid(id.value)==true?cnt++:cnt=0;
+    console.log(cnt);
+    
+    if(cnt==2) 
     {
-    addrow();
-    displayTable();
-    }
+    addrow();}
 }
 
 
@@ -65,23 +99,61 @@ var rowData={
 
 tableData.push(rowData);
 console.log("added!", tableData);
+creatElement(rowData);
 }
-function displayTable(){
-var box ="";
-for(var i =0;i<tableData.length;i++){
-    box+=`                <tr>
-                    <td>${tableData[i].id}</td>
-                    <td>${tableData[i].name}</td>
-                    <td>${tableData[i].age}</td>
-                    <td><button onclick="">Delete</button></td>
-                </tr>
-    `
-    
+function creatElement(input){
+var tr = document.createElement('tr');
+var tdId = document.createElement('td');
+var tdName = document.createElement('td');
+var tdAge = document.createElement('td');
+tdAge.textContent = input.age;
+tdId.textContent = input.id;
+tdName.textContent = input.name;
+    tr.appendChild(tdId);
+    tr.appendChild(tdName);
+    tr.appendChild(tdAge);
+    table1.appendChild(tr);
 }
-table1.innerHTML = box;
+
+//clear input 
+function clear(){
+    fname.value="";
+    fname.style.border="2px solid black";
+    id.value="";
+    id.style.border="2px solid black";
+    age.value="";
 }
 
 //events
 //first
-btn.addEventListener("click",formValidate);
+
+fname.addEventListener('input', function (e) {
+    validateNameInput(e.target.value);
+
+});
+fname.addEventListener('blur', function (e) {
+    validateNameInput(e.target.value);
+
+});
+fname.addEventListener('focus', function (e) {
+    validateNameInput(e.target.value);
+
+});
+
+id.addEventListener('input', function(e){
+    validateid(e.target.value);
+
+})
+id.addEventListener('blur', function(e){
+    validateid(e.target.value);
+
+})
+id.addEventListener('focus', function(e){
+    validateid(e.target.value);
+
+})
+btn.addEventListener('click',function(){
+    formValidate();
+    clear();
+});
 
